6cfd35290737720cf148ddedbb194521b9b26892
Jim Patt & alex caste <student@epicodus.com>
2015-04-22 15:56:15 -0700

    Set specs

---
+++ spec/finder_spec.rb
@@ -0,0 +1,15 @@
+require('rspec')
+require('rps_game.rb')
+require('pry')
+
+describe('String#find') do
+
+#Find a word in the string
+
+#Replace word with second word
+
+#Make sure to only find and replace whole words
+
+#Make sure to take into account partial matches
+
+end
